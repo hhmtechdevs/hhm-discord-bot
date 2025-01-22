072079e5edf04c8b0791cafc3cb2ca336406a381
TheFlippantFox <theflippantfox@gmail.com>
2025-01-22 20:11:21 +0000

    cleanup

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -6,23 +6,20 @@ import { CLIENT_ID, DISCORD_BOT_TOKEN, GUILD_ID } from './config';
 const commands = new Map<string, Command>();
 loadCommands(commands)
 
-// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(DISCORD_BOT_TOKEN);
 
-// and deploy your commands!
 (async () => {
   try {
     console.log(`Started refreshing ${commands.size} application (/) commands.`);
     console.log(...commands)
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
+
+    const data: any = await rest.put(
       Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
       { body: commands },
     );
 
-    console.log(`Successfully reloaded ${data} application (/) commands.`);
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
     console.error(error);
   }
 })();
