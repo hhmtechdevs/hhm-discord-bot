cd942c6c201f5b94e4d85dfe6fa19b2176e0189d
TheFlippantFox <theflippantfox@gmail.com>
2025-01-23 08:32:03 +0000

    changed: constant for theme color

--- src/commands/care/feel.ts
+++ src/commands/care/feel.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction, EmbedBuilder, } from 'discord.js'
 import { Command } from '../../typings/Command'
+import { THEME_COLOR } from '../../config'
 
 const feel: Command = {
   data: {
@@ -7,7 +8,10 @@ const feel: Command = {
     description: 'the feel wheel',
   },
   execute: async (interaction: CommandInteraction) => {
-    const embed = new EmbedBuilder().setColor(0x0099ff).setTitle('The Feel Wheel').setDescription(`Name 2 or 3 feelings that closely describe how you're currently feeling from the good old Feel Wheel:`).setImage('https://i.redd.it/hfhc3k6g12x91.jpg')
+    const embed = new EmbedBuilder().setColor(THEME_COLOR)
+      .setTitle('The Feel Wheel')
+      .setDescription(`Name 2 or 3 feelings that closely describe how you're currently feeling from the good old Feel Wheel:`)
+      .setImage('https://i.redd.it/hfhc3k6g12x91.jpg')
 
     await interaction.reply({ embeds: [embed] })
   },
