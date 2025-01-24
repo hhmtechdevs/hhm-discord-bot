6b51b077c5a5b9a3739ec440204dd9ac2c3a24ae
TheFlippantFox <theflippantfox@gmail.com>
2025-01-24 13:07:50 +0000

    fixed: mentions in /moodmeter

--- src/commands/care/moodmeter.ts
+++ src/commands/care/moodmeter.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, EmbedBuilder, } from 'discord.js'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, EmbedBuilder, } from 'discord.js'
 import { Command } from '../../typings/Command'
 import { CONFIG } from '../../config'
 
@@ -13,9 +13,17 @@ const moodmeter: Command = {
       .setColor(CONFIG.ENotificationColors.PRIMARY_COLOR)
       .setDescription("Good Morning Afternoon Evening. How are we all doing on scale of 1-10? 10 being good.")
 
+    const button = new ButtonBuilder()
+      .setCustomId('moodmeter_response')
+      .setLabel('Respond')
+      .setStyle(ButtonStyle.Primary)
+
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(button)
+
     await interaction.reply({
       content: '@here',
       embeds: [embed],
+      components: [row],
       allowedMentions: { parse: ['everyone'] }
     })
   },
